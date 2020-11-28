b032734193e375be7d4e3b62c4f8f405606ea90b
Donna Samuel <donnasamuel@Donnas-MacBook-Air.local>
2020-11-28 13:00:27 +0200

    Clean: Unwanted code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
 import React, { useRef } from 'react';
 import { Helmet } from 'react-helmet';
-import L from 'leaflet';
-import { Marker } from 'react-leaflet';
-
-import { promiseToFlyTo, getCurrentLocation } from 'lib/map';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
 import Snippet from 'components/Snippet';
 
-import gatsby_astronaut from 'assets/images/gatsby-astronaut.jpg';
 
 const LOCATION = {
   lat: 38.9072,
@@ -18,28 +13,9 @@ const LOCATION = {
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
 
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
 
 const IndexPage = () => {
-  const markerRef = useRef();
-  const mapRef = useRef();
 
   /**
    * mapEffect
@@ -47,33 +23,8 @@ const IndexPage = () => {
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
-  async function mapEffect({ leafletElement } = {}) {
-    if ( !leafletElement ) return;
+  async function mapEffect({ leafletElement: map } = {}) {
 
-    const popup = L.popup({
-      maxWidth: 800,
-    });
-
-    const location = await getCurrentLocation().catch(() => LOCATION );
-
-    const { current = {} } = markerRef || {};
-    const { leafletElement: marker } = current;
-
-    marker.setLatLng( location );
-    popup.setLatLng( location );
-    popup.setContent( popupContentHello );
-
-    setTimeout( async () => {
-      await promiseToFlyTo( leafletElement, {
-        zoom: ZOOM,
-        center: location,
-      });
-
-      marker.bindPopup( popup );
-
-      setTimeout(() => marker.openPopup(), timeToOpenPopupAfterZoom );
-      setTimeout(() => marker.setPopupContent( popupContentGatsby ), timeToUpdatePopupAfterZoom );
-    }, timeToZoom );
   }
 
   const mapSettings = {
@@ -89,8 +40,7 @@ const IndexPage = () => {
         <title>Home Page</title>
       </Helmet>
 
-      <Map ref={mapRef} {...mapSettings}>
-        <Marker ref={markerRef} position={CENTER} />
+      <Map {...mapSettings}>
       </Map>
 
       <Container type="content" className="text-center home-start">
